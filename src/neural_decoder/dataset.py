601696bd67568b3c3326bf980799a089e74380e5
“Jurglex” <ajurgens64@gmail.com>
2025-12-05 00:22:04 -0500

    double phoneme attempt

--- src/neural_decoder/dataset.py
+++ src/neural_decoder/dataset.py
@@ -3,9 +3,11 @@ from torch.utils.data import Dataset
 
 
 class SpeechDataset(Dataset):
-    def __init__(self, data, transform=None):
+    def __init__(self, data, transform=None, pair_mode="none", n_phone_classes=None):
         self.data = data
         self.transform = transform
+        self.pair_mode = pair_mode
+        self.n_phone_classes = n_phone_classes
         self.n_days = len(data)
         self.n_trials = sum([len(d["sentenceDat"]) for d in data])
 
@@ -17,9 +19,36 @@ class SpeechDataset(Dataset):
         for day in range(self.n_days):
             for trial in range(len(data[day]["sentenceDat"])):
                 self.neural_feats.append(data[day]["sentenceDat"][trial])
-                self.phone_seqs.append(data[day]["phonemes"][trial])
                 self.neural_time_bins.append(data[day]["sentenceDat"][trial].shape[0])
-                self.phone_seq_lens.append(data[day]["phoneLens"][trial])
+
+                if self.pair_mode == "none":
+                    self.phone_seqs.append(data[day]["phonemes"][trial])
+                    self.phone_seq_lens.append(data[day]["phoneLens"][trial])
+                else:
+                    seq = data[day]["phonemes"][trial]
+                    L = int(data[day]["phoneLens"][trial])
+                    if self.pair_mode == "overlap":
+                        if L >= 2:
+                            pairs = []
+                            for i in range(L - 1):
+                                p1 = int(seq[i])
+                                p2 = int(seq[i + 1])
+                                bid = (p1 - 1) * self.n_phone_classes + (p2 - 1) + 1
+                                pairs.append(bid)
+                            self.phone_seqs.append(pairs)
+                            self.phone_seq_lens.append(L - 1)
+                        else:
+                            self.phone_seqs.append([])
+                            self.phone_seq_lens.append(0)
+                    elif self.pair_mode == "nonoverlap":
+                        pairs = []
+                        for i in range(0, L - 1, 2):
+                            p1 = int(seq[i])
+                            p2 = int(seq[i + 1])
+                            bid = (p1 - 1) * self.n_phone_classes + (p2 - 1) + 1
+                            pairs.append(bid)
+                        self.phone_seqs.append(pairs)
+                        self.phone_seq_lens.append(len(pairs))
                 self.days.append(day)
 
     def __len__(self):
